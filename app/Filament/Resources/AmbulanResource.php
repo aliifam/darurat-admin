2e062141f6ad2dc33a379901ee8f0bb5d64ebfa7
aliifam <aliif.arief.m@gmail.com>
2023-12-04 11:21:58 +0700

    pemadam resource done

--- app/Filament/Resources/AmbulanResource.php
+++ app/Filament/Resources/AmbulanResource.php
@@ -130,8 +130,8 @@ class AmbulanResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
                 Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make()
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
