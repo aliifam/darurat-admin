ded13a09aff7d48e39305ffaf389dafd5c25f1fb
aliifam <aliif.arief.m@gmail.com>
2023-12-04 12:59:26 +0700

    map header for all

--- app/Filament/Resources/PolisiResource/Pages/ListPolisis.php
+++ app/Filament/Resources/PolisiResource/Pages/ListPolisis.php
@@ -3,12 +3,20 @@
 namespace App\Filament\Resources\PolisiResource\Pages;
 
 use App\Filament\Resources\PolisiResource;
+use App\Filament\Resources\PolisiResource\Widgets\PolisiMap;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPolisis extends ListRecords
 {
     protected static string $resource = PolisiResource::class;
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            PolisiMap::make(),
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
---
+++ app/Filament/Resources/PolisiResource/Widgets/PolisiMap.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources\PolisiResource\Widgets;
+
+use Cheesegrits\FilamentGoogleMaps\Widgets\MapWidget;
+
+class PolisiMap extends MapWidget
+{
+    protected static ?string $heading = 'Peta Kantor Polisi';
+
+    protected int | string | array $columnSpan = '2';
+
+    protected static ?int $sort = 1;
+
+    protected static ?string $pollingInterval = null;
+
+    protected static ?bool $clustering = true;
+
+    protected static ?bool $fitToBounds = true;
+
+    protected static ?int $zoom = 12;
+
+    protected function getData(): array
+    {
+        /**
+         * You can use whatever query you want here, as long as it produces a set of records with your
+         * lat and lng fields in them.
+         */
+        $locations = \App\Models\Polisi::all();
+
+        $data = [];
+
+        foreach ($locations as $location) {
+            /**
+             * Each element in the returned data must be an array
+             * containing a 'location' array of 'lat' and 'lng',
+             * and a 'label' string (optional but recommended by Google
+             * for accessibility.
+             *
+             * You should also include an 'id' attribute for internal use by this plugin
+             */
+            $data[] = [
+                'location'  => [
+                    'lat' => $location->latitude ? round(floatval($location->latitude), static::$precision) : 0,
+                    'lng' => $location->longitude ? round(floatval($location->longitude), static::$precision) : 0,
+                ],
+
+                'label'     => $location->nama,
+
+                'id' => $location->getKey(),
+
+                /**
+                 * Optionally you can provide custom icons for the map markers,
+                 * either as scalable SVG's, or PNG, which doesn't support scaling.
+                 * If you don't provide icons, the map will use the standard Google marker pin.
+                 */
+                'icon' => [
+                    'url' => url('images/polisi.png'),
+                    'type' => 'svg',
+                    'scale' => [35, 35],
+                ],
+            ];
+        }
+
+        return $data;
+    }
+}
